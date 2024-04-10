a535b8a15146509beb00c01afeb5b13be046257a
yugma-wecp <yugma@wecreateproblems.com>
2024-04-10 13:20:38 +0530

    add Anthropic AI, upgrade UI, requirements

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ PDF Chat is a simple yet powerful tool that allows users to engage in conversati
 - **Upload PDF**: Easily upload your PDF file to initiate conversations with your data.
 - **Intelligent Chat**: Utilize OpenAI's language model to ask questions and interact with the content.
 - **Open Source Embedding Model**: The embedding model is open source, eliminating concerns about embedding costs.
-- **Future Expansion**: Expect future updates, including additional options like integration with Anthropic's Claude.
+- **Future Expansion**: Expect future updates, including additional options like integration with Huggingface's open source models.
 
 
 ### Download and Launch:
@@ -25,11 +25,12 @@ Make sure that you have Git installed on your device.
 
 ### How to use:
 
-- **Upload API Key**: Start by providing your OpenAI API key for authentication.
+- **Select preferred AI model**: Start off by selecting your preferred AI model (We have OpenAI's ChatGPT and Anthropic's Claude models as of now)
+- **Upload API Key**: Provide your API key for authentication.
 - **Upload PDF**: Select the PDF file you want to converse with.
 - **Ask Questions**: Engage in meaningful conversations with your PDF data.
 
-**Important Note**: Please ensure that you upload a valid OpenAI API key to enable seamless integration and accurate responses.
+**Important Note**: Please ensure that you upload a valid API key to enable seamless integration and accurate responses.
 
 
 ### Contributions:
@@ -39,7 +40,7 @@ Make sure that you have Git installed on your device.
 
 ### Future Roadmap:
 
-- Integration with Anthropic's Claude for expanded functionality and enhanced user experience.
+- Integration with Huggingface's open source models for expanded, customized functionality and additional cost savings.
 - Additional features and improvements based on user feedback and community contributions.
 
 
--- app.py
+++ app.py
@@ -1,33 +1,58 @@
 import os
 import openai
+import anthropic
 import streamlit as st
-import pickle
 from streamlit_extras.add_vertical_space import add_vertical_space
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import FAISS
-from langchain.embeddings import OpenAIEmbeddings, HuggingFaceEmbeddings
-from langchain.chat_models import ChatOpenAI, ChatAnthropic
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings.huggingface import HuggingFaceEmbeddings
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
 from langchain.chains.question_answering import load_qa_chain
-from langchain.callbacks import get_openai_callback
+from langchain_community.callbacks import get_openai_callback
+import pickle
 
-# EMBEDDINGS MODEL
-# model = 'sentence-transformers/sentence-t5-base' # okayish, not that good
-model = 'sentence-transformers/msmarco-distilbert-base-dot-prod-v3'
+# EMBEDDING MODELS
+# embedding_model = 'sentence-transformers/sentence-t5-base' # okayish, not that good
+embedding_model = 'sentence-transformers/msmarco-distilbert-base-dot-prod-v3' # better than the above
+
+# number of chunks to return from the PDF
+ret_chunks = 10
 
 # sidebar
 with st.sidebar:
-    st.markdown('''
-    # PDF Chatbot
-    ## Instructions
-    1. Upload your OpenAI API key
-    2. Upload your PDF file
-    3. Ask your question
-    4. Get the answer
-    5. Enjoy!
-    ''')
+    st.write("# PDF Chatbot")
 
-    add_vertical_space(5)
+    model_type = st.selectbox(label="Please select your preferred AI model to get started:", placeholder="Select your preferred AI model", options=["OpenAI", 
+    "Anthropic"], label_visibility="hidden", index=None)
+    model = None
+    if model_type == "OpenAI":
+        model = st.radio("Select your preferred OpenAI model", ["gpt-3.5-turbo", "gpt-4"], index=None)
+    elif model_type == "Anthropic":
+        model = st.radio("Select your preferred Anthropic model", ["claude-3-opus-20240229", "claude-3-sonnet-20240229", "claude-3-haiku-20240307"], index=None)
+    
+    add_vertical_space(1)
+    
+    api_key = None
+    if model is not None and model_type is not None:
+        api_key = st.text_input(f"Please enter your {model_type} API Key here:")
+        add_vertical_space(1)
+        if api_key is not None:
+            temperature = st.select_slider("Select the temperature for the model", options=[0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0], value=0.0)
+
+    st.divider()
+    st.markdown('''
+    ## Instructions
+    1. Select your preferred AI model
+    2. Upload your API key
+    3. Upload your PDF file
+    4. Ask your question
+    5. Get the answer
+    6. Repeat!
+    ''')
+    st.divider()
+    add_vertical_space(3)
     st.write("This app was created using [Streamlit](https://streamlit.io/), [Langchain](https://langchain.com/), and [OpenAI](https://openai.com/).")
 
 def extract_text(pdf):
@@ -66,67 +91,57 @@ def api_key_test(api_key):
 # main content
 def main():
     st.title("PDF Chatbot")
-    # st.write("Upload a PDF file to get started!")
-    
-    openai_api_key = st.text_input("OpenAI API Key")
 
-    if not openai_api_key:
-        st.write("Please upload your OpenAI API key to get started.")
-    elif openai_api_key:
-        # st.write("OpenAI API key uploaded successfully!")
-        # st.write(openai_api_key)
+    if model:
+        # api_key = st.text_input(f"{model_type} API Key")
+        if api_key:
+            test_return = api_key_test(api_key)
 
-        test_return = api_key_test(openai_api_key)
+            if test_return == True:
+                pdf = st.file_uploader("Please upload a PDF file to get started", type="pdf")
+                if pdf is not None:
+                    text = extract_text(pdf)
 
-        if test_return == True:
+                    splitter = RecursiveCharacterTextSplitter(
+                        chunk_size=1000,
+                        chunk_overlap=200,
+                        length_function=len
+                    )
 
-            pdf = st.file_uploader("Choose a PDF file", type="pdf")
+                    chunks = splitter.split_text(text=text)
 
-            if pdf is None:
-                st.write("Please upload a PDF file to get started.")
-            elif pdf is not None:
-                # st.write("PDF uploaded successfully!")
-                
-                text = extract_text(pdf)
+                    store_name = pdf.name[:-4]
 
-                splitter = RecursiveCharacterTextSplitter(
-                    chunk_size=1000,
-                    chunk_overlap=200,
-                    length_function=len
-                )
+                    if os.path.exists(f"{store_name}.pkl"):
+                        with open(f"{store_name}.pkl", "rb") as f:
+                            vector_store = pickle.load(f)
+                    else:
+                        embeddings = HuggingFaceEmbeddings(model_name=embedding_model)
+                        vector_store = FAISS.from_texts(chunks, embedding=embeddings)
+                        with open(f"{store_name}.pkl", "wb") as f:
+                            pickle.dump(vector_store, f)
 
-                chunks = splitter.split_text(text=text)
-                # st.write(f"Number of chunks: {len(chunks)}\n\n")
+                    question = st.chat_input("Ask a question:")
+                    if question is not None:
+                        st.chat_message(name="user").write(question)
+                    
+                        docs = vector_store.similarity_search(query=question, k=ret_chunks)
 
-                store_name = pdf.name[:-4]
-                # st.write(store_name)
-                if os.path.exists(f"{store_name}.pkl"):
-                    with open(f"{store_name}.pkl", "rb") as f:
-                        vector_store = pickle.load(f)
-                        # st.write("Index loaded successfully!")
-                else:
-                    embeddings = HuggingFaceEmbeddings(model_name=model)
-                    vector_store = FAISS.from_texts(chunks, embedding=embeddings)
-                    with open(f"{store_name}.pkl", "wb") as f:
-                        pickle.dump(vector_store, f)
+                        # st.write(f"Top {k} most relevant chunks for the question '{question}':")
+                        # for i, doc in enumerate(docs):
+                        #     st.write(f"{i+1}. {doc}")
 
-                    # st.write("Embeddings indexed successfully!")
+                        if model_type == "OpenAI":
+                            chat = ChatOpenAI(openai_api_key=api_key, model=model, temperature=0.0)
+                        elif model_type == "Anthropic":
+                            chat = ChatAnthropic(anthropic_api_key=api_key, model=model, temperature=0.0)
 
-                question = st.text_input("Ask a question:")
-
-                if question:
-                    docs = vector_store.similarity_search(query=question, k=3)
-
-                    chat = ChatOpenAI(openai_api_key=openai_api_key, model="gpt-3.5-turbo", temperature=0)
-                    chain = load_qa_chain(llm=chat, chain_type="stuff")
-                    with get_openai_callback() as cb:
+                        chain = load_qa_chain(llm=chat, chain_type="stuff")
                         response = chain.run(input_documents=docs, question=question)
-                        print(cb)
-                        print(f"*"*100)
-                        st.write(response)
-                
-        else:
-            st.write("Invalid OpenAI API key. Please try again.")
+                        st.chat_message(name="ai").write(response)
+                    
+            else:
+                st.write("Invalid API key. Please try again.")
 
 if __name__ == '__main__':
     main()
