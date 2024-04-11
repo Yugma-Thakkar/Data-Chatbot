2f7762a57fbd9f73d1b6eceb043f9e90facf7b85
Yugma-Thakkar <yugma007.t@gmail.com>
2024-04-11 18:11:52 +0530

    add api key verification

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
-import openai
+from openai import OpenAI
 import anthropic
 import streamlit as st
+import pickle
 from streamlit_extras.add_vertical_space import add_vertical_space
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -11,7 +12,6 @@ from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
 from langchain.chains.question_answering import load_qa_chain
 from langchain_community.callbacks import get_openai_callback
-import pickle
 
 # EMBEDDING MODELS
 # embedding_model = 'sentence-transformers/sentence-t5-base' # okayish, not that good
@@ -20,7 +20,6 @@ embedding_model = 'sentence-transformers/msmarco-distilbert-base-dot-prod-v3' # 
 # number of chunks to return from the PDF
 ret_chunks = 10
 
-# sidebar
 with st.sidebar:
     st.write("# PDF Chatbot")
 
@@ -28,7 +27,7 @@ with st.sidebar:
     "Anthropic"], label_visibility="hidden", index=None)
     model = None
     if model_type == "OpenAI":
-        model = st.radio("Select your preferred OpenAI model", ["gpt-3.5-turbo", "gpt-4"], index=None)
+        model = st.radio("Select your preferred OpenAI model", ["gpt-3.5-turbo", "gpt-4", "gpt-4-turbo"], index=None)
     elif model_type == "Anthropic":
         model = st.radio("Select your preferred Anthropic model", ["claude-3-opus-20240229", "claude-3-sonnet-20240229", "claude-3-haiku-20240307"], index=None)
     
@@ -62,43 +61,62 @@ def extract_text(pdf):
         text += page.extract_text()
     return text
 
-# later on, figure out how to test the OpenAI API key without having to run every time
-# test.txt -> <API_KEY> = True | False
-def api_key_test(api_key):
-    # try:
-    #     response = openai.ChatCompletion.create(
-    #         model="gpt-3.5-turbo",
-    #         messages=[
-    #             {"role": "system", "content": "This is just a test."},
-    #             {"role": "user", "content": "Reply with 'True' if you can see this message."},
-    #         ],
-    #         max_tokens=5,
-    #         api_key=api_key,
-    #         temperature=0.0
-    #     )
-        
-    #     print(response.choices[0].message.content)
-    #     if response.choices[0].message.content == "True":
-    #         # create a file to store the response
-    #         with open("test.txt", "w") as f:
-    #             f.write(response.choices[0].message.content)
-    #         return True
+@st.cache_data
+def openai_api_key_test(api_key):
+    client = OpenAI(api_key=api_key)
 
-    # except Exception as e:
-    #     return Exception
-    return True
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "This is an authentication test. Don't reply with anything other than 'True' or 'False'."},
+            {"role": "user", "content": "Return True if you can see this message."},
+        ],
+        max_tokens=5,
+        temperature=0.0
+    )
+    
+    print(f"{model_type} - {response.choices[0].message.content}")
+    if response.choices[0].message.content == "True":
+        return True
+    else:
+        return False
+    
+@st.cache_data
+def anthropic_api_key_test(api_key):
+    client = anthropic.Client(api_key=api_key)
 
-# main content
+    response = client.messages.create(
+        model="claude-3-haiku-20240307",
+        system="This is an authentication test. Don't reply with anything other than 'True' or 'False'.",
+        messages=[
+            {"role": "user", "content": "Return True if you can see this message."} 
+        ],
+        max_tokens=5,
+        temperature=0.0
+    )
+
+    print(f"{model_type} - {response.content[0].text}")
+    if response.content[0].text == "True":
+        return True
+    else:
+        return False
+
+
 def main():
     st.title("PDF Chatbot")
 
-    if model:
-        # api_key = st.text_input(f"{model_type} API Key")
-        if api_key:
-            test_return = api_key_test(api_key)
+    if model:        
 
+        if api_key:
+
+            if model_type == "OpenAI":
+                test_return = openai_api_key_test(api_key)
+            elif model_type == "Anthropic":
+                test_return = anthropic_api_key_test(api_key)
+                
             if test_return == True:
                 pdf = st.file_uploader("Please upload a PDF file to get started", type="pdf")
+
                 if pdf is not None:
                     text = extract_text(pdf)
 
@@ -109,7 +127,6 @@ def main():
                     )
 
                     chunks = splitter.split_text(text=text)
-
                     store_name = pdf.name[:-4]
 
                     if os.path.exists(f"{store_name}.pkl"):
@@ -122,9 +139,9 @@ def main():
                             pickle.dump(vector_store, f)
 
                     question = st.chat_input("Ask a question:")
+
                     if question is not None:
                         st.chat_message(name="user").write(question)
-                    
                         docs = vector_store.similarity_search(query=question, k=ret_chunks)
 
                         # st.write(f"Top {k} most relevant chunks for the question '{question}':")
@@ -138,10 +155,16 @@ def main():
 
                         chain = load_qa_chain(llm=chat, chain_type="stuff")
                         response = chain.run(input_documents=docs, question=question)
-                        st.chat_message(name="ai").write(response)
-                    
-            else:
+                        st.chat_message(name="ai").write(response)  
+                    else:
+                        st.write("Please ask a question.")
+
+            elif test_return == False:
                 st.write("Invalid API key. Please try again.")
+
+            else:
+                st.write("Something went wrong. Please try again.")
+
 
 if __name__ == '__main__':
     main()
